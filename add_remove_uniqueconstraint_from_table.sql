98ddd0ec95592f6dd86c2db954f325ebfd4b86be
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-07-30 14:51:21 +1000

    intital commit

--- add_remove_uniqueconstraint_from_table.sql
+++ add_remove_uniqueconstraint_from_table.sql
@@ -13,15 +13,15 @@ DROP INDEX <constraintname>;
 SELECT * FROM pets;
 
 
-
+-- Add unique constraint
 ALTER TABLE pets
 ADD CONSTRAINT u_species UNIQUE(species); -- Now only unique values allowed in species column
 
+-- Remove unique constraint
+ALTER TABLE pets
+DROP INDEX u_species;
 
-
-
-
-
+DESCRIBE pets;
 
 
 
